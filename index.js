ae766bd26b7b873efbf7030bafd9800993c91ee7
Markéta Hornerová <hornerova.m@gmail.com>
2021-04-18 14:14:16 +0200

    Hra zakladni zobrazeni x o v JS

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+let player = 'circle';
+
+const addSymbol = (event) => {
+  if (player === 'circle') {
+    event.target.classList.add('board__field--circle');
+    player = 'cross';
+  } else {
+    event.target.classList.add('board__field--cross');
+    player = 'circle';
+  }
+};
+
+const btnsElm = document.querySelectorAll('.single-game-button');
+for (let i = 0; i < btnsElm.length; i++) {
+  btnsElm[i].addEventListener('click', addSymbol);
+}
