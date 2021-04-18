82a9cabbf1e738734894e513fe8858ab4329d9f7
Markéta Hornerová <hornerova.m@gmail.com>
2021-04-18 17:07:59 +0200

    Pridan disabled ale cely se to rozjebalo

--- index.js
+++ index.js
@@ -2,15 +2,20 @@
 
 let player = 'circle';
 
+const symbolElm = document.querySelector('.symbol');
+
 const addSymbol = (event) => {
   if (player === 'circle') {
     event.target.classList.add('board__field--circle');
     player = 'cross';
+    symbolElm.src = 'podklady/cross.svg';
   } else {
     event.target.classList.add('board__field--cross');
     player = 'circle';
+    symbolElm.src = 'podklady/circle.svg';
   }
   event.target.removeEventListener('click', addSymbol);
+  event.target.disabled = true;
 };
 
 const btnsElm = document.querySelectorAll('.single-game-button');
