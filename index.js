21c170ecf6cce652798f36d127b580379fcffa76
Markéta Hornerová <hornerova.m@gmail.com>
2021-05-02 15:19:50 +0200

    Hotova rada

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ const addSymbol = (event) => {
     player = 'circle';
     symbolElm.src = 'podklady/circle.svg';
   }
-  // event.target.removeEventListener('click', addSymbol);
   event.target.disabled = true;
+  isWinningMove(event.target);
 };
 
 const btnsElm = document.querySelectorAll('.single-game-button');
@@ -23,10 +23,60 @@ for (let i = 0; i < btnsElm.length; i++) {
   btnsElm[i].addEventListener('click', addSymbol);
 }
 
-const getSymbol = (field) => {
-  if (field.classList.contains('board__field--circle')) {
+// Detekce výherního tahu
+
+const getSymbol = (btnsElm) => {
+  if (btnsElm.classList.contains('board__field--circle')) {
     return 'circle';
-  } else if (field.classList.contains('board__field--cross')) {
+  } else if (btnsElm.classList.contains('board__field--cross')) {
     return 'cross';
   }
+};
+
+const boardSize = 10;
+const fields = document.querySelectorAll('.single-game-button');
+
+const getField = (row, column) => btnsElm[row * boardSize + column];
+
+const getPosition = (btnsElm) => {
+  let fieldIndex = 0;
+  while (fieldIndex < fields.length && btnsElm !== fields[fieldIndex]) {
+    fieldIndex += 1;
+  }
+  return {
+    row: Math.floor(fieldIndex / boardSize),
+    column: fieldIndex % boardSize,
+  };
+};
+
+const isWinningMove = (btnsElm) => {
+  const origin = getPosition(btnsElm);
+  const symbol = getSymbol(btnsElm);
+
+  // Řada
+  let i;
+  //vlevo
+  let inRow = 1;
+  i = origin.column;
+  while (i > 0 && symbol === getSymbol(getField(origin.row, i - 1))) {
+    inRow += 1;
+    i -= 1;
+  }
+
+  //vpravo
+  i = origin.column;
+  while (
+    i < boardSize - 1 &&
+    symbol === getSymbol(getField(origin.row, i + 1))
+  ) {
+    inRow++;
+    i++;
+  }
+
+  if (inRow >= 5) {
+    console.log('vyhrál');
+    return true;
+  }
+
+  // Sloupec
 };
